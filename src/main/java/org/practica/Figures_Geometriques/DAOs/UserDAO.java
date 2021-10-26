1a9bca44f1b577139042739136780a91f53c15b5
Pablo <Pablo20101997@hotmail.es>
2021-10-26 15:31:49 +0200

    Implementación de las clases UserDAO y User

--- src/main/java/org/practica/Figures_Geometriques/DAOs/UserDAO.java
+++ src/main/java/org/practica/Figures_Geometriques/DAOs/UserDAO.java
@@ -1,7 +1,10 @@
 package org.practica.Figures_Geometriques.DAOs;
 
+import org.practica.Figures_Geometriques.Models.User;
+
 import java.util.List;
 
 public interface UserDAO {
-
+    List<User> getUsers();
+    User getUser(String username);
 }
--- src/main/java/org/practica/Figures_Geometriques/Models/User.java
+++ src/main/java/org/practica/Figures_Geometriques/Models/User.java
@@ -1,7 +1,23 @@
 package org.practica.Figures_Geometriques.Models;
 
 public class User {
-
     String username;
     String password;
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
