e0b178832609a2b8c3e009d9731887ea61fee15e
Pablo <Pablo20101997@hotmail.es>
2021-10-26 15:47:55 +0200

    Implementacion del metodo de inicio de sesion (sin DB) en LoginController y el metodo que resuelve el login en LoginService

--- src/main/java/org/practica/Figures_Geometriques/Controllers/LoginController.java
+++ src/main/java/org/practica/Figures_Geometriques/Controllers/LoginController.java
@@ -1,15 +1,19 @@
 package org.practica.Figures_Geometriques.Controllers;
 
+import org.practica.Figures_Geometriques.Services.LoginService;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(value = "/login")
 public class LoginController extends HttpServlet {
+    LoginService loginService = new LoginService();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -21,6 +25,16 @@ public class LoginController extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String username = req.getParameter("username");
         String password = req.getParameter("password");
+
+        if(loginService.userOk(username,password)){
+            HttpSession session = req.getSession();
+            session.setAttribute("auth",true);
+            session.setAttribute("username",username);
+
+            RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/jsp/loginok.jsp");
+            dispatcher.forward(req,resp);
+            return;
+        }
 
         req.setAttribute("message","username or password incorrect");
         RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/jsp/LoginOk.jsp");
--- src/main/java/org/practica/Figures_Geometriques/Services/LoginService.java
+++ src/main/java/org/practica/Figures_Geometriques/Services/LoginService.java
@@ -1,4 +1,19 @@
 package org.practica.Figures_Geometriques.Services;
 
+import org.practica.Figures_Geometriques.DAOs.UserDAO;
+import org.practica.Figures_Geometriques.DAOs.UserDAOImpl;
+import org.practica.Figures_Geometriques.Models.User;
+
 public class LoginService {
+    UserDAO userDAO = new UserDAOImpl();
+
+    public boolean userOk(String username, String password){
+        User user = userDAO.getUser(username);
+        if (user == null) return false;
+
+        if (password.equals(user.getPassword())){
+            return true;
+        }
+        return false;
+    }
 }
