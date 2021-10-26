0a0ea3322d5f81670def5909e5ca895daff579b8
Pablo <Pablo20101997@hotmail.es>
2021-10-26 15:40:48 +0200

    Creacion y implementacion de un Usuario en UserDAOImpl

---
+++ src/main/java/org/practica/Figures_Geometriques/DAOs/UserDAOImpl.java
@@ -0,0 +1,20 @@
+package org.practica.Figures_Geometriques.DAOs;
+
+import org.practica.Figures_Geometriques.Models.User;
+
+import java.util.List;
+
+public class UserDAOImpl implements UserDAO {
+    @Override
+    public List <User> getUsers() {
+        return null;
+    }
+
+    @Override
+    public User getUser(String username) {
+        User user = new User();
+        user.setUsername("pablo");
+        user.setPassword("1234");
+        return user;
+    }
+}
