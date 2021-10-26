8d340f0bed548bef41b9419943c0e266589bc7f5
Pablo <Pablo20101997@hotmail.es>
2021-10-26 16:57:46 +0200

    Creacion del odelo de figura.

---
+++ src/main/java/org/practica/Figures_Geometriques/Models/Figure.java
@@ -0,0 +1,58 @@
+package org.practica.Figures_Geometriques.Models;
+
+public class Figure {
+    String name;
+    String typeFigure;
+    String color;
+    int size;
+    int coordenateX;
+    int coordenateY;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTypeFigure() {
+        return typeFigure;
+    }
+
+    public void setTypeFigure(String typeFigure) {
+        this.typeFigure = typeFigure;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getCoordenateX() {
+        return coordenateX;
+    }
+
+    public void setCoordenateX(int coordenateX) {
+        this.coordenateX = coordenateX;
+    }
+
+    public int getCoordenateY() {
+        return coordenateY;
+    }
+
+    public void setCoordenateY(int coordenateY) {
+        this.coordenateY = coordenateY;
+    }
+}
