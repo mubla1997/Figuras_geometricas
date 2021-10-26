49a2f849b25c80d55b2fff81e320a68a0fe147c5
Pablo <Pablo20101997@hotmail.es>
2021-10-26 16:33:03 +0200

    Actualizamos el loginOK para redirigir directamente a la pagina indice y authfilter 2.0

---
+++ src/main/java/org/practica/Figures_Geometriques/Filters/AuthFilter.java
@@ -0,0 +1,28 @@
+package org.practica.Figures_Geometriques.Filters;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/indice/*")
+public class AuthFilter extends HttpFilter {
+    @Override
+    protected void doFilter(HttpServletRequest req, HttpServletResponse resp, FilterChain chain) throws IOException, ServletException {
+        HttpSession session = req.getSession();
+
+        if (session.getAttribute("auth") != null){
+            req.setAttribute("username",session.getAttribute("username"));
+            chain.doFilter(req,resp);
+
+            return;
+        }
+        resp.sendError(401,"Acceso no autorizado");
+    }
+}
